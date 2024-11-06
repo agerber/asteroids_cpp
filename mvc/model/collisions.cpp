3df8a54b842c40b12031ca40371894f0c378177c
Kelvin Wekesa <wekesa01.kelvin@gmail.com>
2024-11-06 06:27:10 +0300

    maximize precision of collision detection

--- mvc/model/collisions.cpp
+++ mvc/model/collisions.cpp
@@ -1,5 +1,21 @@
 #include "collisions.h"
 
+bool approximatelyEqual(float a, float b, float epsilon) {
+  return fabs(a - b) <= ((fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
+
+bool essentiallyEqual(float a, float b, float epsilon) {
+  return fabs(a - b) <= ((fabs(a) > fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
+
+bool definitelyGreaterThan(float a, float b, float epsilon) {
+  return (a - b) > ((fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
+
+bool definitelyLessThan(float a, float b, float epsilon) {
+  return (b - a) > ((fabs(a) < fabs(b) ? fabs(b) : fabs(a)) * epsilon);
+}
+
 float CalculateDistance(const sf::Vector2f& p1, const sf::Vector2f& p2) {
   float diffY = p1.y - p2.y;
   float diffX = p1.x - p2.x;
--- mvc/model/collisions.h
+++ mvc/model/collisions.h
@@ -1,25 +1,38 @@
 #ifndef COLLISIONS_H
 #define COLLISIONS_H
 #include <cmath>
-#include "Movable.h" 
+
+#include "Movable.h"
 template <class A, class B>
-class Collisions{
-	A &obj1;
-	B &obj2;
-public:
-	Collisions(A &obj1, B &obj2);
-	bool intersect();
+class Collisions {
+  A &obj1;
+  B &obj2;
+
+ public:
+  Collisions(A &obj1, B &obj2);
+  bool intersect();
 };
 
-float CalculateDistance(const sf::Vector2f& p1, const sf::Vector2f& p2);
-template <class A, class B>
-Collisions<A,B>::Collisions(A &obj1, B& obj2) : obj1{obj1}, obj2{obj2} {}
+bool approximatelyEqual(float a, float b, float epsilon);
+bool essentiallyEqual(float a, float b, float epsilon);
+bool definitelyGreaterThan(float a, float b, float epsilon);
+bool definitelyLessThan(float a, float b, float epsilon);
+float CalculateDistance(const sf::Vector2f &p1, const sf::Vector2f &p2);
 
 template <class A, class B>
-bool Collisions<A,B>::intersect() {
-  if (CalculateDistance(obj1->getCenter(), obj2->getCenter()) <=
-      std::abs(obj1->getRadius() + obj2->getRadius()))
-    return true;
+Collisions<A, B>::Collisions(A &obj1, B &obj2) : obj1{obj1}, obj2{obj2} {}
+
+template <class A, class B>
+bool Collisions<A, B>::intersect() {
+  bool aprox = approximatelyEqual(
+      CalculateDistance(obj1->getCenter(), obj2->getCenter()),
+      std::abs(obj1->getRadius() + obj2->getRadius()),
+      std::numeric_limits<float>::epsilon());
+  bool less = definitelyLessThan(
+      CalculateDistance(obj1->getCenter(), obj2->getCenter()),
+      std::abs(obj1->getRadius() + obj2->getRadius()),
+      std::numeric_limits<float>::epsilon());
+  if (aprox || less) return true;
 
   return false;
 }
